f544f6936e6bd6ac171c2b0fa6df21c6e6abc029
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 22:43:45 -0400

    adds NavBr.js file

---
+++ src/Components/NavBar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {Link, NavLink, withRouter} from 'react-router-dom'
+import { connect, useSelector } from 'react-redux'
+
+function NavBar(props) {
+    const current_user = useSelector(state => {
+        return state.user
+    })
+
+    const clearToken = () => {
+        localStorage.clear()
+    }
+    const logOut = () => {
+        props.clearUser()
+        clearToken()
+        props.history.push('/')
+    }
+    return (
+       
+        <>
+        {!current_user.id ?
+             <div id="header">
+                <h1 className="header-name"> Admin Page</h1>
+             </div> : 
+             <>
+                <div id="header">
+                    <h1 className="header-name"> Admin Page</h1>
+                </div>
+                <nav id="nav-bar">
+                    <div className="link-div">
+                        <Link className="link" to="/">Main Page</Link>
+                        <Link className="link" to="/addCards">
+                            Add Cards
+                        </Link>
+                        <NavLink className="link" to="/">
+                            <button id="logout" onClick={logOut}>Logout</button>
+                        </NavLink>
+                    </div>
+                </nav>
+             </>
+        }
+       
+        </>
+    )
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        clearUser: () => {
+            dispatch({
+                type: 'CLEAR_USER'
+            })
+        }
+    }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(NavBar))
