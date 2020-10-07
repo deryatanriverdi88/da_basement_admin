78e06be8af2f7125bb43749bb3a04ed9f6af58b5
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 22:52:47 -0400

    adds AddCards class component

--- src/Pages/AddCards.js
+++ src/Pages/AddCards.js
@@ -1,2 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
+class AddCards extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
