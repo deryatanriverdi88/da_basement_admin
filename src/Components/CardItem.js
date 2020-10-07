6b902178e9c64a561d679320e132c2562886985d
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 14:37:08 -0400

    adds ul to list detail of a card within CardItem.js

--- src/Components/CardItem.js
+++ src/Components/CardItem.js
@@ -4,6 +4,11 @@ export default function CardItem(props) {
   const {name, img_url} = props.card
   return (
     <div className="card-item">
+      <ul>
+        <li> <span>Name :</span> { name } </li>
+        <li> <img src={img_url} alt={name}/></li>
+        <button  className="x" onClick={props.handleClick}> <span> ❌ </span></button>
+      </ul>
     </div>
   )
 }
