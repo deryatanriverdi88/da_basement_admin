6ff687c446b7c5124a614e7cd507c37ced744435
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 23:05:26 -0400

    adds handleSubmit func. to Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -15,6 +15,34 @@ class Login extends Component {
         })
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:3000/login', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+        .then(res => res.json())
+        .then(admin => {
+            console.log(admin)
+            localStorage.setItem('token', admin.token)
+            if(admin.token){
+                this.props.setUser(admin.current_user)
+                this.props.history.push('/myCards')
+            } else {
+                this.setState({
+                    errors: admin.error
+                })
+            }
+        })
+    }
+
     render() {
         return(
             <div className="login-container">
