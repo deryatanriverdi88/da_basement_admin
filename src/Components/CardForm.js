ac52f8064fccf127097b322fdb4df57c2d4bed84
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 14:21:28 -0400

    imports CardItem for CardForm

--- src/Components/CardForm.js
+++ src/Components/CardForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import CardItem from "./CardItem"
 
 export default class CardForm extends Component {
     render() {
