94fb2aadc3ecce7df2beae45a9f9ec0f206048c6
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 23:29:10 -0400

    adds Switch to render routes in Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,20 @@ import Pages from './Pages'
 const Routes = () => {
     return(
         <div>
-
+            <Switch>
+                <Route exact path='/'>
+                    {localStorage.token ?
+                        <Redirect to="/myCards" component={Pages.MyCards}/> :
+                        <Pages.Login/>
+                    }
+                </Route>
+                <Route exact path='/myCards' component={Pages.MyCards}>
+                    {!localStorage.token ?
+                    <Redirect to="/" component={Pages.Login}/> :
+                    <Pages.MyCards/>}
+                </Route>
+                <Route exact path='/addCards' component={Pages.AddCards}></Route>
+            </Switch>
         </div>
     )
 }
