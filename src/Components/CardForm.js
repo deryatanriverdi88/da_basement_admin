93e8343d67661d7d046833fe639cfd851a9bf1f8
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 14:21:58 -0400

    imports withRouter from react-router-dom for CardForm

--- src/Components/CardForm.js
+++ src/Components/CardForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import CardItem from "./CardItem"
+import { withRouter } from 'react-router-dom'
 
 export default class CardForm extends Component {
     render() {
