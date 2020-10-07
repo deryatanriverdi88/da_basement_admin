bc53eddbc6469a05bd82ec02d67e4bfd0df11c16
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 22:53:19 -0400

    adds componentDidMount function to AddCards.js

--- src/Pages/AddCards.js
+++ src/Pages/AddCards.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class AddCards extends Component {
+
+    componentDidMount(){
+        fetch('http://localhost:3000/last_ten')
+        .then(res=>res.json())
+        .then(cardData => {
+            this.props.getCards(cardData)
+        })
+    }
+
     render() {
         return (
             <div>
