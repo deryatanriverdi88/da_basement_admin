64468036666cb6b70e446b1099831e6e45ad5f2c
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 14:22:34 -0400

    adds amount, cardView, and card to state of CardForm

--- src/Components/CardForm.js
+++ src/Components/CardForm.js
@@ -4,6 +4,12 @@ import CardItem from "./CardItem"
 import { withRouter } from 'react-router-dom'
 
 export default class CardForm extends Component {
+    state={
+        amount: null,
+        cardView: false,
+        card: {}
+    }
+
     render() {
         return (
             <div>
