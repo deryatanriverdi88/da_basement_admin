10b7a63f156f0095187782a421f08c5f78abdb08
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 23:01:11 -0400

    adds class component for Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,3 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {withRouter} from 'react-router-dom'
+
+class Login extends Component {
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Login
