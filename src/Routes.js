7fafd5efcc4733654c40a122bffd70b71ff8f9c5
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 23:28:10 -0400

    adds functional component for Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Pages from './Pages'
+
+const Routes = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Routes
