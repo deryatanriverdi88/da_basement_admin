b7cf47fc5dea19764aad47a31f1180cfd21803d4
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 22:43:21 -0400

    adds App.js file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {} from 'react';
+import Routes from './Routes'
+import {withRouter} from 'react-router-dom'
+import './App.css';
+import {connect} from 'react-redux'
+import NavBar from './Components/NavBar'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
