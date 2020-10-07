f9a048261e76f2f73a7bbd81f3216a7bb78ed83f
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 23:17:29 -0400

    exports user, favoriteCards and addCards within combineReducers in rootReducers.js

--- src/Reducers/rootReducers.js
+++ src/Reducers/rootReducers.js
@@ -2,3 +2,9 @@ import { combineReducers } from 'redux'
 import user from './userReducer'
 import favoriteCards from './favoriteCardsReducer'
 import addCards from './addCardReducer'
+
+export default combineReducers({
+    user: user,
+    favoriteCards: favoriteCards,
+    addCards: addCards
+})
