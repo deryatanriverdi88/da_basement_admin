ab0797a5b9e5260dfde6b3c5029b64f097a49463
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 23:02:15 -0400

    adds className of login-container for div in Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -5,7 +5,7 @@ import {withRouter} from 'react-router-dom'
 class Login extends Component {
     render() {
         return(
-            <div>
+            <div className="login-container">
             </div>
         )
     }
