0094127a93f447c90f274d720974f8edb2aac683
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 23:11:05 -0400

    imports React, and Component from react and adds class components for MyCards.js

---
+++ src/Pages/MyCards.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class MyCards extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
