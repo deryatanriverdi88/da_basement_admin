c060766c1c268581b203124f06923846a691334c
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-09 23:31:15 -0400

    adds amount of card, and edit button to change the amount within MyCardItem.js

--- src/Components/MyCardItem.js
+++ src/Components/MyCardItem.js
@@ -10,7 +10,22 @@ export default class MyCardItem extends Component {
     render() {
         return (
             <>
-                <tr className="row" key={this.props.card.id}></tr>
+                <tr className="row" key={this.props.card.id}>
+                <td>{this.props.editForm && this.props.editCard.id === this.props.card.id ?
+                        <input type="number" min="0" value={this.props.amount} onChange={this.props.handleChange}/>
+                        :
+                        <p>{this.props.card.amount}</p>
+                    }
+                    {this.props.editForm && this.props.editCard.id === this.props.card.id ?
+                       <>
+                       <button className="edit-card" onClick={this.props.handleEditSubmit}> Submit </button>
+                       <button onClick={(e) => this.props.handleClick(e, this.props.card)}> X </button>
+                       </>
+                        :
+                       <button className="edit-card" onClick={(e) => this.props.handleClick(e, this.props.card)}> Edit </button>
+                    }
+                    </td>
+                </tr>
             </>
         )
     }
