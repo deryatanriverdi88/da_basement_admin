177635f3ddcf196784575464777e1cba887f7f1c
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-09 23:31:59 -0400

    beutifies the code

--- src/Components/MyCardItem.js
+++ src/Components/MyCardItem.js
@@ -11,19 +11,19 @@ export default class MyCardItem extends Component {
         return (
             <>
                 <tr className="row" key={this.props.card.id}>
-                <td>{this.props.editForm && this.props.editCard.id === this.props.card.id ?
-                        <input type="number" min="0" value={this.props.amount} onChange={this.props.handleChange}/>
-                        :
-                        <p>{this.props.card.amount}</p>
-                    }
-                    {this.props.editForm && this.props.editCard.id === this.props.card.id ?
-                       <>
-                       <button className="edit-card" onClick={this.props.handleEditSubmit}> Submit </button>
-                       <button onClick={(e) => this.props.handleClick(e, this.props.card)}> X </button>
-                       </>
-                        :
-                       <button className="edit-card" onClick={(e) => this.props.handleClick(e, this.props.card)}> Edit </button>
-                    }
+                    <td>{this.props.editForm && this.props.editCard.id === this.props.card.id ?
+                            <input type="number" min="0" value={this.props.amount} onChange={this.props.handleChange}/>
+                            :
+                            <p>{this.props.card.amount}</p>
+                        }
+                        {this.props.editForm && this.props.editCard.id === this.props.card.id ?
+                        <>
+                        <button className="edit-card" onClick={this.props.handleEditSubmit}> Submit </button>
+                        <button onClick={(e) => this.props.handleClick(e, this.props.card)}> X </button>
+                        </>
+                            :
+                        <button className="edit-card" onClick={(e) => this.props.handleClick(e, this.props.card)}> Edit </button>
+                        }
                     </td>
                 </tr>
             </>
