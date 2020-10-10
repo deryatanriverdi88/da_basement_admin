680099c87631f8cb411d5d4c1bb1c2d996895f48
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-09 23:22:20 -0400

    adds optionsCursorTrueWithMargin for ReactHover within MyCardItem.js" "

--- src/Components/MyCardItem.js
+++ src/Components/MyCardItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactHover, {Trigger, Hover} from 'react-hover'
+const optionsCursorTrueWithMargin = {followCursor:true, shiftX: -280, shiftY: -280}
 
 export default class MyCardItem extends Component {
     render() {
