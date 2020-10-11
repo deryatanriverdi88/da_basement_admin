fbeefa73a69a5043371656eeeeee40c6712bfcd8
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-10 20:20:14 -0400

    adds a div to notify that card is added with card name within PopUp.js

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function PopUp() {
     return (
         <div className="pop-up ">
+            <div className ="pop-up-fade">
+                <p>{props.card.magic_the_gatherig_card.name} is added ✅ </p>
+            </div>
         </div>
     )
 }
