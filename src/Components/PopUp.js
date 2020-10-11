937b35c9ef26cd66f6d6f17accef0244d633a708
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-10 20:22:00 -0400

    adds props to PopUp.js

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function PopUp() {
+export default function PopUp(props) {
     return (
-        <div className="pop-up ">
+        <div className="pop-up">
             <div className ="pop-up-fade">
                 <p>{props.card.magic_the_gatherig_card.name} is added ✅ </p>
             </div>
