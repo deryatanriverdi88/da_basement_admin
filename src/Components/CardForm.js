b03e6020333190c21b08578b67ca30c4d795d44a
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-10 21:29:10 -0400

    adds checkbox field for normal attribute within CardForm.js

--- src/Components/CardForm.js
+++ src/Components/CardForm.js
@@ -91,35 +91,58 @@ class CardForm extends Component {
                  <div className="card-form">
                     {this.state.errors ? <p>{this.state.errors[0]}</p> : null}
                  <h3 className="card-name">{ name }</h3>
-                 <form onSubmit={this.handleSubmit}>
-                         <label htmlFor="amount"> <span className="amount"> Amount </span></label>
-                            <input
-                                id="amount"
-                                name="amount"
-                                type="number"
-                                onChange={this.handleChange}
-                                value={this.state.amount}
-                            />
-                       { foil_low_price || foil_mid_price || foil_high_price || foil_market_price ?
-                       <><label htmlFor="foil"><span className="foil"> Foiled </span></label>
-                            <input
-                                id="foil"
-                                nam="foil"
-                                type="checkbox"
-                                onChange={this.handleFoilChange}
-                                value={this.state.foil}
-                            /> </> : null
-                            }
-                            <input
-                                type="hidden"
-                                id="user"
-                                name="userId"
-                            />
-                            <input
-                                type="hidden"
-                                id="card"
-                                name="magic_the_gatherig_card_id"
-                            />
+                 <form onSubmit={this.handleSubmit} id="add-card-form">
+                     <div className="form-fields">
+                        <label htmlFor="amount"> <span className="amount"> Amount </span></label>
+                                <input
+                                    id="amount"
+                                    name="amount"
+                                    type="number"
+                                    onChange={this.handleChange}
+                                    value={this.state.amount}
+                                />
+                                { foil_low_price || foil_mid_price || foil_high_price || foil_market_price ?
+                                <>
+                                <label htmlFor="foil"><span className="foil"> Foiled </span></label>
+                                        <input
+                                            id="foil"
+                                            name="foil"
+                                            disabled={this.state.foilDisable}
+                                            type="checkbox"
+                                            onChange={this.handleCardVersionChange}
+                                            value={this.state.foil}
+                                        />
+                                </>
+                                :
+                                null
+                                }
+                                { normal_low_price || normal_mid_price || normal_high_price || normal_market_price ?
+                                <>
+                                <label htmlFor="normal"><span className="normal"> Normal </span></label>
+                                        <input
+                                            id="normal"
+                                            name="normal"
+                                            type="checkbox"
+                                            disabled={this.state.normalDisable}
+                                            onChange={this.handleCardVersionChange}
+                                            value={this.state.normal}
+                                        />
+                                </>
+                                :
+                                null
+                                }
+                                <input
+                                    type="hidden"
+                                    id="user"
+                                    name="userId"
+                                />
+                                <input
+                                    type="hidden"
+                                    id="card"
+                                    name="magic_the_gatherig_card_id"
+                                />
+                            </div>
+
                            <input className="add-your-card" type="submit" value="Add to your favorite cards!" />
                      </form>
                     <button className="x" onClick={this.props.handleClick}> <span > ❌ </span></button>
