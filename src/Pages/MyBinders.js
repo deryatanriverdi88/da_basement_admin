8a9cfa5609fe1291ff578dc4aff20bafc92db4d0
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 00:24:06 -0400

    adds class function for MyBinders.js

---
+++ src/Pages/MyBinders.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class MyBinders extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
