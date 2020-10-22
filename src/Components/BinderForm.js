6d895ee9e48bf88c71d795c997449b9b0d9a89a5
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 01:01:01 -0400

    adds handleChange function to BinderForm.js

--- src/Components/BinderForm.js
+++ src/Components/BinderForm.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import {withRouter} from 'react-router-dom'
 
 function BinderForm({current_user, history, setBinderForm}) {
+    const handleOnChange =(e) => {
+        setBinderName(e.target.value)
+    }
     const [binderName, setBinderName ] = useState("")
     return (
         <div>
