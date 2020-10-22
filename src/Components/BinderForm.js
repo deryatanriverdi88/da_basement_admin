77e1c1d748ffe0f85d35c047a4a81c30b4b4d94b
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 01:00:36 -0400

    adds binderName to state of BinderForm.js

--- src/Components/BinderForm.js
+++ src/Components/BinderForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {withRouter} from 'react-router-dom'
 
 function BinderForm({current_user, history, setBinderForm}) {
+    const [binderName, setBinderName ] = useState("")
     return (
         <div>
         </div>
