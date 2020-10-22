63b4449f6ece4552959a59ffb43b1f94ed4c7dd1
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 00:25:10 -0400

    imports MyBinderItem for MyBinders.js

--- src/Pages/MyBinders.js
+++ src/Pages/MyBinders.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import MyBinderItem from '../Components/MyBinderItem'
 
 export default class MyBinders extends Component {
     render() {
