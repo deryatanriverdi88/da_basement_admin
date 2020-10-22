d60a0b1f38ccadce8feb7076cd76b4675b9ea12d
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 00:48:39 -0400

    adds optionsCursorTrueWithMargin variable to MyBinderItem.js

--- src/Components/MyBinderItem.js
+++ src/Components/MyBinderItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactHover, {Trigger, Hover} from 'react-hover'
+const optionsCursorTrueWithMargin = {followCursor:true, shiftX: -280, shiftY: -280}
 
 export default function MyBinderItem() {
     return (
