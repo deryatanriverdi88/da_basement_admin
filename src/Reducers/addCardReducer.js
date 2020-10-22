6ea65ab7433e7ba87b74a0b1362a7e4c3bd48c82
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 00:17:59 -0400

    adds CLEAR_USER func. for addCardReducer.js

--- src/Reducers/addCardReducer.js
+++ src/Reducers/addCardReducer.js
@@ -2,6 +2,8 @@ export default function addCardReducer(state=[], {type, payload}){
     switch (type) {
         case 'GET_CARDS':
             return payload
+        case 'CLEAR_CARDS':
+            return []
         default:
             return state
     }
