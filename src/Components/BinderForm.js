d3c094f34480c9983be745a55d58f1a7a71dd2dd
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 01:01:56 -0400

    adds handleOnSubmit function to BinderForm.js

--- src/Components/BinderForm.js
+++ src/Components/BinderForm.js
@@ -2,10 +2,32 @@ import React, { useState } from 'react'
 import {withRouter} from 'react-router-dom'
 
 function BinderForm({current_user, history, setBinderForm}) {
+    const [binderName, setBinderName ] = useState("")
     const handleOnChange =(e) => {
         setBinderName(e.target.value)
     }
-    const [binderName, setBinderName ] = useState("")
+
+    const handleOnSubmit = (e) =>{
+        e.preventDefault()
+        fetch('https://da-basement-games-api.herokuapp.com/binders', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body:JSON.stringify({
+                user_id: current_user.id,
+                name: binderName
+            })
+        })
+        .then(res => res.json())
+        .then(binderObj => {
+            history.push({pathname: `/addCards/${binderObj.name}`,
+            state: {binder: binderObj},
+            })
+            setBinderForm(false)
+        })
+    }
     return (
         <div>
         </div>
