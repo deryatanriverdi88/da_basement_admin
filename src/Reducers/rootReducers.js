ea230ecf3719bf7f74dc0be991ef1902ae551e6f
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 00:19:20 -0400

    imports addBinders within rootReducer.js

--- src/Reducers/rootReducers.js
+++ src/Reducers/rootReducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import user from './userReducer'
 import favoriteCards from './favoriteCardsReducer'
 import addCards from './addCardReducer'
+import addBinders from './addBinderReducer'
 
 export default combineReducers({
     user: user,
