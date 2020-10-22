eec116ef0d8d6375628bb573b943601b24f5f10d
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 01:26:01 -0400

    adds CLEAR_FAVORITE_CARDS function favoriteCardsReducer

--- src/Reducers/favoriteCardsReducer.js
+++ src/Reducers/favoriteCardsReducer.js
@@ -2,6 +2,8 @@ export default function favoriteCardsReducer(state=[], {type, payload}){
     switch (type) {
         case 'SET_FAVORITE_CARDS':
             return payload
+        case 'CLEAR_FAVORITE_CARDS':
+            return []
         default:
             return state
     }
