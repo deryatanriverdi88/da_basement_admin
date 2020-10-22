24cf2a26f7309b5fa3045bd43f4cef916914f700
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-22 00:25:52 -0400

    imports withRouter from react-router-dom for MyBinders.js

--- src/Pages/MyBinders.js
+++ src/Pages/MyBinders.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import MyBinderItem from '../Components/MyBinderItem'
+import {withRouter} from 'react-router-dom'
 
 export default class MyBinders extends Component {
     render() {
