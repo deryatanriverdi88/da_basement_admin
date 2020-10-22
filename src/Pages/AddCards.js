24f3d8d354bd4b846be6b15637084ca8101cec67
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-21 23:42:25 -0400

    adds cardNames, cardName and binder to state of AddCards.js

--- src/Pages/AddCards.js
+++ src/Pages/AddCards.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import CardForm from '../Components/CardForm'
 import PopUp from '../Components/PopUp'
-import ReactHover, {Trigger, Hover} from 'react-hover'
-
-const optionsCursorTrueWithMarginForImage = {followCursor:true, shiftX: -280, shiftY: -280}
-const optionsCursorTrueWithMarginForFoil = {followCursor:true, shiftX: -320, shiftY: -320}
 
 class AddCards extends Component {
     state = {
         searchValue: "",
         card: {},
         cardForm: false,
-        popUp: false
+        popUp: false,
+        cardNames: [],
+        cardName: "",
+        binder: {}
     }
 
     handleChange = (event) => {
