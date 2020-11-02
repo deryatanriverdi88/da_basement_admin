3d2ef4509e29c131e550c94d1a6f491e39d3dccb
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-02 16:48:45 -0500

    wraps component with a div classnamed background-for-pop-up-index within PopUp.js

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function PopUp(props) {
     return (
-        <div className="pop-up">
-            {console.log(props.card)}
-            <div className ="pop-up-fade">
-               <p>{props.card.name} is added !</p>
-               <img src={props.card.img_url} alt={props.card.name}/>
-            </div>
+        <div className="background-for-pop-up-index">
+            <div className="pop-up">
+                {console.log(props.card)}
+                <div className ="pop-up-fade">
+                <p>{props.card.name} is added !</p>
+                <img src={props.card.img_url} alt={props.card.name}/>
+                </div>
+             </div>
         </div>
     )
 }
