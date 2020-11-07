e2a974e36c547cee4a28bbe504597341561155bb
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-07 01:53:22 -0500

    updates PopUp component

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -6,7 +6,9 @@ export default function PopUp(props) {
             <div className="pop-up">
                 {console.log(props.card)}
                 <div className ="pop-up-fade">
-                <p>{props.card.name} is added !</p>
+                <p>{props.amount} of {props.card.name} is added !</p>
+                <p> Foil : {props.foil ? "Foil" : "Non foil"}</p>
+                {/* {props.errors ? <p>{props.errors} </p> : <p>{props.card.name} is added !</p>} */}
                 <img src={props.card.img_url} alt={props.card.name}/>
                 </div>
              </div>
