9d54fe51172fceff934631283734bba7338a8917
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-10 22:16:55 -0500

    removes import of PopUp.js

--- src/Pages/AddCards.js
+++ src/Pages/AddCards.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import CardForm from '../Components/CardForm'
-import PopUp from '../Components/PopUp'
 
 class AddCards extends Component {
     state = {
