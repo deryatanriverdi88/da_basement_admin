a318acff5dcd78f662289369553df51b4ec30cf9
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-10 22:15:21 -0500

    removes console.log within PopUp.js

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -5,7 +5,6 @@ export default function PopUp(props) {
         <div className="background-for-pop-up-index">
              {console.log(props.card.foil)}
             <div className="pop-up">
-                {console.log(props.card)}
                 <div className ="pop-up-fade">
                 <p>{props.amount} of {props.card.name} is added !</p>
                 <p> Foil : {props.foil ? "Foil" : "Non foil"}</p>
