e30e67880575f102d9f9018b5e52a5085ce5c563
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-10 22:15:36 -0500

    removes error messages from popUp.js

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -8,7 +8,6 @@ export default function PopUp(props) {
                 <div className ="pop-up-fade">
                 <p>{props.amount} of {props.card.name} is added !</p>
                 <p> Foil : {props.foil ? "Foil" : "Non foil"}</p>
-                {/* {props.errors ? <p>{props.errors} </p> : <p>{props.card.name} is added !</p>} */}
                 <img src={props.card.img_url} alt={props.card.name}/>
                 </div>
              </div>
