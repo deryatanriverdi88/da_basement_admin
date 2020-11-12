1899b4fcc9ca19db7bf697f8d782952d709062ad
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-11 23:15:58 -0500

    updates fetchCards func for MyCards.js

--- src/Pages/MyCards.js
+++ src/Pages/MyCards.js
@@ -32,14 +32,17 @@ class MyCards extends Component {
         const setNames = this.props.favoriteCards.filter(card=>{
             return !this[card.group_name]? this[card.group_name] = true :false
         })
+          this.setState({
+            setNames: setNames.sort((a,b) => a.group_name > b.group_name ? 1 : -1)
+          })
     }
-
 
     handleClick = (e, card) => {
         this.setState({
             amount: card.amount,
             editForm: !this.state.editForm,
-            editCard: card
+            editCard: card,
+            foil: card.foil
         })
     }
 
