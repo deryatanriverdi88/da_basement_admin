72140d9f9b2039cad8355b5523aa3ff79c392591
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-11-20 23:44:30 -0500

    updates namings

--- src/Pages/MyBinders.js
+++ src/Pages/MyBinders.js
@@ -474,9 +474,9 @@ class MyBinders extends Component {
                 {
                     this.state.binderItem.id ?
                     <>
-                        <button onClick={this.handleAddCardClick}> Add cards to this binder </button>
-                        <button onClick={this.handleEditBinderClick}> Edit this binder </button>
-                        <button onClick={this.handleBinderDelete}> Delete Collection </button>
+                        <button onClick={this.handleAddCardClick}> Add cards </button>
+                        <button onClick={this.handleEditBinderClick}> Edit </button>
+                        <button onClick={this.handleBinderDelete}> Delete </button>
                     </> :
                     null
                 }
